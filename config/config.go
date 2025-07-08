8d75f691f8891057f1f1ce37c3cbd31bf40c2f95
Ruturaj Patil <ruturaj.dilippatil@gmail.com>
2025-07-09 00:43:26 +0530

    init: project structure

---
+++ config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type Config map[string][]string
+
+func LoadConfig(path string) (Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
